c6073830b1cdf9507ba8fc7bf57fceda1f66bfab
willems davy <joyrider3774@hotmail.com>
2025-04-05 08:13:43 +0200

    support custom width & height

--- src/blockdude.cpp
+++ src/blockdude.cpp
@@ -2853,6 +2853,14 @@ int main(int argc, char **argv)
             WINDOW_WIDTH = ORIG_WINDOW_WIDTH * 5;
             WINDOW_HEIGHT = ORIG_WINDOW_HEIGHT * 5;
         }
+
+        if(strcasecmp(argv[i], "-w") == 0)
+			if(i+1 < argc)
+				WINDOW_WIDTH = atoi(argv[i+1]);
+		
+		if(strcasecmp(argv[i], "-h") == 0)
+			if(i+1 < argc)
+				WINDOW_HEIGHT = atoi(argv[i+1]);
     }
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO ) == 0)
